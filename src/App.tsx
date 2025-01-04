c5dbe8befcf826b2de3104cb46ebd949c14b265d
Minje Sung <minje813@dgu.ac.kr>
2025-01-04 20:28:04 +0900

    feat: 카드 컴포넌트 App에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.css';
+import Card from './components/Card';
 
 function App() {
-
   return (
     <div>
       <h1>민카드</h1>
+      <Card />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
