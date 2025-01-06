9d784d42f9c58a4a9f9fff9452f1910085ecd645
Minje Sung <minje813@dgu.ac.kr>
2025-01-06 17:50:08 +0900

    feat: Card 컴포넌트 story 생성 완료

---
+++ src/stories/Card/Card.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Card from "./Card";
+
+const meta = {
+  title: 'MinCard',
+  component: Card,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Card>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
