d215435ab91a86f245eb20d687a6ec1161bfd5cb
Minje Sung <minje813@dgu.ac.kr>
2025-02-03 16:18:00 +0900

    fix: Card Component를 실제 Component 위치로 연결

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,46 @@
-function Card() {
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  width: 220px;
+  height: 310px;
+  transition: 0.1s transform;
+`;
+
+const CardContent = styled.div`
+  width: 220px;
+  height: 310px;
+  background-color: #ddd;
+  background-size: cover;
+`;
+
+const Card = () => {
+  const [style, setStyle] = useState({});
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const x = e.nativeEvent.offsetX;
+    const y = e.nativeEvent.offsetY;
+    const rotateX = (-2 / 29) * y + 10;
+    const rotateY = (1 / 10) * x - 10;
+
+    setStyle({
+      style,
+      transform: `perspective(300px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
+    });
+  };
+
+  const handleMouseOut = () => {
+    setStyle({
+      style,
+      transform: 'rotateX(0deg) rotateY(0deg)',
+    });
+  };
+
   return (
-    <div>
-      <p>카드 컴포넌트임</p>
-    </div>
+    <CardContainer style={style} onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
+      <CardContent />
+    </CardContainer>
   );
-}
+};
 
 export default Card;
