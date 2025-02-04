82da3acace33e2212cdbee355646404d6d2eedae
Minje Sung <minje813@dgu.ac.kr>
2025-02-05 02:39:40 +0900

    feat: 마우스를 따라 빛이 움직이는 효과 추가
    CardOverlay를 이용해 마우스를 따라 빛이 움직이는 효과 추가
    희귀 카드의 경우 부가적인 효과를 넣을 예정

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,13 +6,19 @@ const CardContainer = styled.div`
   position: relative;
   width: 250px;
   height: 390px;
+  border: 1px solid #030303;
+  border-radius: 10px;
   transition: 0.1s transform;
+  overflow: hidden;
 `;
 
 const CardOverlay = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
+  background-image: radial-gradient(circle, rgba(255, 255, 255, 0.4) 10%, transparent 50%);
+  pointer-events: none; /* 마우스 이벤트 차단 */
+  transition: all 0.1s;
 `;
 
 const CardContent = styled.div`
@@ -25,29 +31,39 @@ const CardContent = styled.div`
 
 const Card = () => {
   const [cardStyle, setCardStyle] = useState({});
+  const [overlayStyle, setOverlayStyle] = useState({});
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const x = e.nativeEvent.offsetX;
     const y = e.nativeEvent.offsetY;
     const rotateX = (-1 / 39) * y + 5;
     const rotateY = (1 / 25) * x - 5;
+    const posX = x - 125
+    const posY = y - 195
 
     setCardStyle({
-      style: cardStyle,
       transform: `perspective(300px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
     });
+
+    setOverlayStyle({
+      display: 'block',
+      transform: `translateX(${posX}px) translateY(${posY}px)`,
+    })
   };
 
   const handleMouseOut = () => {
     setCardStyle({
-      style: cardStyle,
       transform: 'rotateX(0deg) rotateY(0deg)',
     });
+
+    setOverlayStyle({
+      transform: `translateX(0px) translateY(0px)`,
+    })
   };
 
   return (
     <CardContainer style={cardStyle} onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
-      <CardOverlay />
+      <CardOverlay style={overlayStyle} />
       <CardContent />
     </CardContainer>
   );
