07148a59592e44cd2c36cb6b66c4d0684305fe18
Minje Sung <minje813@dgu.ac.kr>
2025-02-05 21:46:45 +0900

    fix: 카드 이미지를 bang 카드가 아닌 tarot 카드로 변경

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import CardImage from '../assets/images/card/01_bang.png';
+import CardImage from '../assets/images/card/Magician.jpg';
 
 const CardContainer = styled.div`
   position: relative;
   width: 250px;
-  height: 390px;
+  height: 420px;
   border: 1px solid #030303;
   border-radius: 10px;
   transition: 0.1s transform;
@@ -23,7 +23,7 @@ const CardOverlay = styled.div`
 
 const CardContent = styled.div`
   width: 250px;
-  height: 390px;
+  height: 420px;
   background-size: cover;
   background-image: url(${CardImage});
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
@@ -36,10 +36,10 @@ const Card = () => {
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const x = e.nativeEvent.offsetX;
     const y = e.nativeEvent.offsetY;
-    const rotateX = (-1 / 39) * y + 5;
+    const rotateX = (-1 / 42) * y + 5;
     const rotateY = (1 / 25) * x - 5;
     const posX = x - 125
-    const posY = y - 195
+    const posY = y - 210
 
     setCardStyle({
       transform: `perspective(300px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
