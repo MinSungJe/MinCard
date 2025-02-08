6cdd0d7b23395146b4463a26cc36de1fd1eedcbf
Minje Sung <minje813@dgu.ac.kr>
2025-02-09 08:04:33 +0900

    feat: 반응형 레이아웃 구현
    display: grid가 아닌 display: flex를 이용해 카드가 화면에 따라 다르게 표시되도록 구현

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import Card from './Card';
 
 const Container = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  row-gap: 20px;
-  column-gap: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 15px;
 `;
 
 const CardContainer = styled.div`
